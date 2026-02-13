739da4714392f1892be01f2639417699ec5a830c
JOMON SAMUEL THOMAS <josamvkd@gmail.com>
2026-02-14 03:06:56 +0400

    first commit

---
+++ src/main/java/com/josamtechie/SecurityExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.josamtechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/josamtechie/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.josamtechie.repository;
+
+import com.josamtechie.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
